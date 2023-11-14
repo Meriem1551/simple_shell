663e8b68ff46e4bd68e0e55c137c2f7255eee6a9
Meriem1551 <meriem200409@gmail.com>
2023-11-14 08:16:45 -0800

    Get environment

--- errors1.c
+++ errors1.c
@@ -12,7 +12,7 @@ int _erratoi(char *s)
 	unsigned long int result = 0;
 
 	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
+		s++;
 	for (i = 0;  s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
